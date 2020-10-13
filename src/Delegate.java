0c830fbf9132419031c6dff449c5f673cef195be
dchalbyshev <dchalbyshev@gmail.com>
2020-10-13 16:50:24 +0300

    with interface

--- src/Delegate.java
+++ src/Delegate.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class Delegate {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Реализация при помощи interface");
+        Painter painter = new Painter();
+        painter.setGrafic(new Circle());
+        painter.draw();
+        painter.setGrafic(new Squre());
+        painter.draw();
+    }
 }
+    interface  Grafics{
+        void draw();
+    }
+
+    class Circle implements  Grafics{
+
+        @Override
+        public void draw() {
+            System.out.println("Рисуем круг");
+        }
+    }
+
+      class Squre implements Grafics{
+
+
+        @Override
+        public void draw() {
+            System.out.println("Рисуем квадрат");
+        }
+    }
+
+    class Painter{
+      Grafics grafic;
+      void  setGrafic(Grafics grafic){
+          this.grafic =  grafic;
+
+      }
+       void draw(){
+          grafic.draw();
+       }
+    }
+
+
+
